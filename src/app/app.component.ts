ab0d0fa25ec20a6a38273711f357a293f74c6cf0
Roberto Avelino <roberto.avelino97@gmail.com>
2022-12-07 12:41:06 -0400

    sending the data from appcomponent.ts sending the dada from the appcomponent.ts to it's children address.component and features.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,26 +13,23 @@ export class AppComponent {
   public data: AppData = {
     features: {
       sectionTitle: 'Destaques',
-      feature1: {
-        image: 'feature1.png',
+      features: [{
+        image: './assets/images/feature1.png',
         title: 'Destaque 1',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dolor arcu, malesuada eget posuere et.'
-      },
-      feature2: {
-        image: 'feature2.png',
+      }, {
+        image: './assets/images/feature2.png',
         title: 'Destaque 2',
         text: 'Aenean laoreet, felis id sollicitudin fringilla, leo orci iaculis eros, et volutpat nunc lacus ut sapien.'
-      },
-      feature3: {
-        image: 'feature3.png',
+      }, {
+        image: './assets/images/feature3.png',
         title: 'Destaque 3',
         text: 'Aenean non eros congue leo consectetur fermentum. Quisque ut dignissim tortor, eget porttitor magna.'
-      },
-      feature4: {
-        image: 'feature4.png',
+      }, {
+        image: './assets/images/feature4.png',
         title: 'Destaque 4',
         text: 'Duis id odio dapibus, finibus tortor eget, cursus nunc. Morbi egestas nisl orci, in cursus ipsum cursus et.'
-      }
+      }]
     },
     about: {
       sectionTitle: 'Sobre a empresa',
@@ -60,7 +57,7 @@ export class AppComponent {
     district: "Conjunto Castelo Branco",
     zipCode: "69911-262"
   }
-  
+
 
   onElementCreated(element: string): void {
     console.log('Elemento criado:', element);
--- src/app/components/features/features.component.ts
+++ src/app/components/features/features.component.ts
@@ -8,8 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class FeaturesComponent implements OnInit {
 
-  @Input() public features!: FeatureData;
-  
+  @Input() public features!: FeatureData[];
+
   constructor() { }
 
   ngOnInit() {
